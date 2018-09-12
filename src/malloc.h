869a1a4726cf87002242ab4a527d5440ca0b41bf
maxkl <max@maxkl.de>
2018-09-13 01:47:46 +0200

    Wrap functions in `extern "C" { ... }` blocks for C++ compatibility

--- src/malloc.h
+++ src/malloc.h
@@ -3,7 +3,15 @@
 
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void *malloc(size_t size);
 void free(void *ptr);
 void *calloc(size_t nmemb, size_t size);
 void *realloc(void *ptr, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
--- src/print.h
+++ src/print.h
@@ -1,6 +1,10 @@
 
 #pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void printi(int);
 void printd(double);
 #ifndef _STDIO_H_
@@ -9,3 +13,7 @@ void printf(float);
 void printc(char);
 void prints(const char *);
 void printptr(const void *);
+
+#ifdef __cplusplus
+}
+#endif
