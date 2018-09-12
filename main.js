60d5100a5592ff9211184503418594aa24991128
maxkl <max@maxkl.de>
2018-09-13 00:29:26 +0200

    Update WebAssembly loading to WebAssembly.instantiateStreaming(...)

--- main.js
+++ main.js
@@ -1,42 +1,37 @@
 (function (Module) {
 	'use strict';
 
-	function fetchAndCompile(url) {
-		return fetch(url)
-			.then(function (response) { return response.arrayBuffer(); })
-			.then(function (buffer) { return WebAssembly.compile(buffer); });
-	}
-
 	function main() {
-		return fetchAndCompile('build/main.wasm').then(function (module) {
-			var imports = {};
-			imports.env = {};
+		var imports = {};
+		imports.env = {};
 
-			for(var i = 0; i < Module.libraries.length; i++) {
-				var lib = Module.libraries[i];
-				if(lib.import) {
-					lib.import(imports.env);;
+		for (var i = 0; i < Module.libraries.length; i++) {
+			var lib = Module.libraries[i];
+			if (lib.import) {
+				lib.import(imports.env);
+			}
+		}
+
+		return WebAssembly.instantiateStreaming(fetch('build/main.wasm'), imports)
+			.then(function (result) {
+				var instance = result.instance;
+
+				if (!instance.exports.memory) {
+					throw new Error('Module does not export its linear memory');
 				}
-			}
 
-			var instance = new WebAssembly.Instance(module, imports);
+				Module.memory = instance.exports.memory;
 
-			if(!instance.exports.memory) {
-				throw new Error('Module does not export its linear memory');
-			}
-
-			Module.memory = instance.exports.memory;
-
-			for(var i = 0; i < Module.libraries.length; i++) {
-				var lib = Module.libraries[i];
-				if(lib.init) {
-					lib.init(instance);
+				for (var i = 0; i < Module.libraries.length; i++) {
+					var lib = Module.libraries[i];
+					if (lib.init) {
+						lib.init(instance);
+					}
 				}
-			}
 
-			window.instance = instance;
-			console.log('Module instantiated');
-		});
+				window.instance = instance;
+				console.log('Module instantiated');
+			});
 	}
 
 	main().catch(function (err) { console.error(err); });
