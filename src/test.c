84af8ffce72c3adbb5740dff3f0c1658b463d0df
maxkl <max@maxkl.de>
2018-09-13 01:16:46 +0200

    Update test code

--- src/test.c
+++ src/test.c
@@ -3,9 +3,21 @@
 #include "mm.h"
 
 __attribute__((visibility("default")))
-void test() {
-	char *str = malloc(sizeof(*str) * 10);
-	prints("malloc returned ");
-	printptr(str);
-	printc('\n');
+void *test_malloc(size_t size) {
+    return malloc(size);
+}
+
+__attribute__((visibility("default")))
+void test_free(void *ptr) {
+    free(ptr);
+}
+
+__attribute__((visibility("default")))
+void *test_calloc(size_t nmemb, size_t size) {
+    return calloc(nmemb, size);
+}
+
+__attribute__((visibility("default")))
+void *test_realloc(void *ptr, size_t size) {
+    return realloc(ptr, size);
 }
